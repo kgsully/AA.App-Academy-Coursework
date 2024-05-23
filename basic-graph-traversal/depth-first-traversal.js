1b11305cdbc555d295697648174f2c8db242bbed
kgsully <ken.sully@gmail.com>
2024-05-23 16:30:21 -0600

    completed basic graph traversal exercises (breadth and dempth first)

---
+++ basic-graph-traversal/depth-first-traversal.js
@@ -0,0 +1,42 @@
+const adjList = {
+    1: [2, 5],
+    2: [1, 3, 5],
+    3: [2, 4],
+    4: [3, 5, 6],
+    5: [1, 2, 4],
+    6: [4]
+  }
+
+  function printDepthFirst(start) {
+    // your code here
+
+    const stack = [];
+    const visited = new Set();
+
+    stack.push(start);
+    visited.add(start);
+
+    while (stack.length > 0) {
+
+        let currentNode = stack.pop();
+
+        console.log(currentNode);
+
+        adjList[currentNode].forEach((el) => {
+            if (!visited.has(el)) {
+                stack.push(el);
+                visited.add(el);
+            }
+        });
+    }
+  }
+
+  console.log("First Test:")
+  printDepthFirst(3); // Prints 1 through 6 in Depth-first order, starting with 3
+                      // One possible solution:  3, 4, 6, 5, 1, 2
+  console.log("Second Test:")
+  printDepthFirst(6); // Prints 1 through 6 in Depth-first order, starting with 6
+                      // One possible solution:  6, 4, 5, 2, 1, 3
+  console.log("Third Test:")
+  printDepthFirst(4); // Prints 1 through 6 in Depth-first order, starting with 4
+                      // One possible solution:  4, 6, 5, 2, 1, 3
