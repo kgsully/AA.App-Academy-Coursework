aba803360018ad1403e8fabeb1457d463b94a12b
kgsully <ken.sully@gmail.com>
2024-07-10 14:50:54 -0600

    completed catsagram practice phase 1 exercise

---
+++ practice-for-week-09-catsagram-long-practice-day-1/public/index.js
@@ -0,0 +1,54 @@
+// Your code here
+
+function initPage() {
+    const pageBody = document.querySelector("body");
+
+    // create h1 header
+    const mainHeader = document.createElement("h1");
+    mainHeader.id = "page-header";
+    mainHeader.innerText = "Catsagram";
+
+    pageBody.appendChild(mainHeader);
+
+    fetchNewCat();
+}
+
+function fetchNewCat() {
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then((res) => {
+            console.log(res);
+            return res.json();
+        })
+        .then (data => {
+            console.log(data);
+            console.log(data[0].url);
+            if(data[0].url) {
+                createCatCard(data[0].url);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+function createCatCard(url) {
+    const catCardDiv = document.createElement('div');
+    catCardDiv.classList.add("cat-card-container");
+
+    const catCardHeader = document.createElement('h2');
+    catCardHeader.classList.add("cat-card-header");
+    catCardHeader.innerText = "Cat Picture";
+
+    const catCardImg = document.createElement("img");
+    catCardImg.classList.add("cat-card-img");
+    catCardImg.src = url;
+    catCardImg.alt = "cat picture";
+
+    catCardDiv.appendChild(catCardHeader);
+    catCardDiv.appendChild(catCardImg);
+
+    const pageBody = document.querySelector("body");
+    pageBody.appendChild(catCardDiv);
+}
+
+window.onload = initPage;
