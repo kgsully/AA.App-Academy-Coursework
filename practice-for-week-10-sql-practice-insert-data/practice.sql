47e455baecb142c1092df35a87866ac4bb95854c
kgsully <ken.sully@gmail.com>
2024-07-19 12:43:56 -0600

    completed sql insert and select practice exercises

---
+++ practice-for-week-10-sql-practice-insert-data/practice.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE friends (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO friends (first_name, last_name)
+VALUES
+    ('Amy', 'Pond'),
+    ('Rose', 'Tyler'),
+    ('Martha', 'Jones'),
+    ('Donna', 'Noble'),
+    ('River', 'Song');
+
+-- De-bug the following SQL statement
+-- INSERT INTO friends (id, first_name, last_name)
+-- VALUES
+--  (5, 'Jenny', 'Who');
+
+-- Issue with unique primary key for id, remove it from the insert statement to allow
+-- it to auto-increment.
+INSERT INTO friends (first_name, last_name)
+VALUES
+    ('Jenny', 'Who');
