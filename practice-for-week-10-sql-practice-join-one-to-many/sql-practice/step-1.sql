aae128f022c0860a56bda763a4ac019cf2304b3e
Ken Sully <ken.sully@gmail.com>
2024-07-24 11:30:50 -0600

    completed sql join one to many practice exercise

---
+++ practice-for-week-10-sql-practice-join-one-to-many/sql-practice/step-1.sql
@@ -0,0 +1,9 @@
+-- Step 1
+-- JOIN the tables, matching FOREIGN KEYs to the corresponding PRIMARY KEY.
+-- Your code here
+
+-- SELECT bands.name, albums.title FROM bands
+--     JOIN albums ON (albums.band_id = bands.id);
+
+SELECT bands.name, albums.title FROM albums
+    JOIN bands ON (albums.band_id = bands.id);
