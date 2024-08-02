20edf7248f1cbfeaed31b0422201e60f77407943
Ken Sully <ken.sully@gmail.com>
2024-08-02 10:20:39 -0600

    completed combining express sequelize short practice exercise

---
+++ practice-for-week-11-combining-express-sequelize-short-practice/server/config/database.js
@@ -0,0 +1,41 @@
+module.exports = {
+  development: {
+    storage: process.env.DB_FILE,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    benchmark: true,
+    logQueryParameters: true,
+    typeValidation: true,
+    // logging: false
+  },
+};
+
+
+
+
+
+
+// // DEFAULT CONFIGURATION PROVIDED BY RUNNING >npx sequelize init -
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
