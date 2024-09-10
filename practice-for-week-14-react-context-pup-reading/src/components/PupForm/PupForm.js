7ab977be0fed535bf9e7c9b078961889f2b830dd
Ken Sully <ken.sully@gmail.com>
2024-09-10 15:39:49 -0600

    Completed React Context Walkthrough - Pup Reading practice exercise

---
+++ practice-for-week-14-react-context-pup-reading/src/components/PupForm/PupForm.js
@@ -0,0 +1,37 @@
+import { useContext, useState } from 'react';
+// import { PupContext } from '../../context/PupContext';
+import { usePuppyType } from '../../context/PupContext';
+import banana from '../../pups/banana-pup.jpg';
+import sleepy from '../../pups/sleepy-pup.jpg';
+import speedy from '../../pups/speedy-pup.jpg';
+
+function PupForm() {
+  const [selectedPup, setSelectedPup] = useState('select');
+  // const {puppyType, setPuppyType} = useContext(PupContext);
+  const { puppyType, setPuppyType } = usePuppyType();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setPuppyType(selectedPup);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <select
+        name="pup"
+        onChange={e => setSelectedPup(e.target.value)}
+        value={selectedPup}
+      >
+        <option value="select" disabled>Select a pup!</option>
+        <option value={speedy}>Speedy Pup</option>
+        <option value={banana}>Banana Pup</option>
+        <option value={sleepy}>Sleepy Pup</option>
+      </select>
+      <button>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default PupForm;
