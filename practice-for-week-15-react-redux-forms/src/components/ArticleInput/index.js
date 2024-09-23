b07af070c47985719f6d112c58f1be9c0388b96d
Ken Sully <ken.sully@gmail.com>
2024-09-23 13:20:11 -0600

    completed react redux forms practice exercise

---
+++ practice-for-week-15-react-redux-forms/src/components/ArticleInput/index.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import { addArticle } from '../../store/articleReducer';
+import './ArticleInput.css';
+
+const ArticleInput = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [newArticle, setNewArticle] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (submitted) {
+      dispatch(addArticle(newArticle));
+      setSubmitted(() => false);
+    }
+  },[submitted, newArticle, dispatch]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && body && imageUrl) {
+      const newArticle = {
+        id: nanoid(),
+        title,
+        body,
+        imageUrl
+      };
+      setNewArticle(() => newArticle);
+      setSubmitted(true);
+      reset();
+    } else {
+      alert('Cannot submit incomplete article');
+    }
+  };
+
+  const reset = () => {
+    setTitle('');
+    setImageUrl('');
+    setBody('');
+  };
+
+  return (
+    <div className='inputBox'>
+      <h1>Create Article</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          placeholder='Title'
+          name='title'
+        />
+        <input
+          type='text'
+          onChange={(e) => setImageUrl(e.target.value)}
+          value={imageUrl}
+          placeholder='Image URL'
+          name='imageUrl'
+        />
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          name='body'
+          placeholder='Add your entry'
+          rows='10'
+        ></textarea>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ArticleInput;
