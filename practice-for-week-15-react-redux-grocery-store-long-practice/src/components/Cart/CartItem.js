68eb383e172664bfd8252505bcead857a33fa114
Ken Sully <ken.sully@gmail.com>
2024-09-25 12:54:42 -0600

    WIP Added remove cart item functionality to React-Redux grocery store long practice exercise

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/components/Cart/CartItem.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/components/Cart/CartItem.js
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeFromCart } from '../../store/cart';
 
 function CartItem({ item }) {
   const [count, setCount] = useState(item.count);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setCount(item.count);
   }, [item.count]);
+
+  const handleRemoveItem = () => {
+    dispatch(removeFromCart(item.id));
+  }
 
   return (
     <li className="cart-item">
@@ -28,6 +35,7 @@ function CartItem({ item }) {
         </button>
         <button
           className="cart-item-button"
+          onClick={handleRemoveItem}
         >
           Remove
         </button>
--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/cart.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/cart.js
@@ -1,4 +1,5 @@
 const ADD_TO_CART = 'cart/ADD_TO_CART';
+const REMOVE_FROM_CART = 'cart/REMOVE_FROM_CART';
 
 export function addToCart(produceId) {
     return {
@@ -7,17 +8,26 @@ export function addToCart(produceId) {
     }
 }
 
-function cartReducer(state = {}, action) {
-    const newState = {
-        ...state,
-        [action.produceId] : {
-            id: action.produceId,
-            count: 1
-        }
+export function removeFromCart(produceId) {
+    return {
+        type: REMOVE_FROM_CART,
+        produceId
     }
+}
+
+function cartReducer(state = {}, action) {
+    // create new object to mutate and leave original state object alone
+    const newState = {...state};
 
     switch(action.type) {
         case ADD_TO_CART:
+            newState[action.produceId] = {
+                id: action.produceId,
+                count: 1
+            }
+            return newState;
+        case REMOVE_FROM_CART:
+            delete newState[action.produceId];
             return newState;
         default:
             return state;
