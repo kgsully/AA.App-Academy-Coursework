37d3270f34f395476dee63c8f0b7231b9294ba84
Ken Sully <ken.sully@gmail.com>
2024-11-08 15:14:56 -0700

    WIP Completed Python Knight's Travail long practice Phase 2

---
+++ practice-for-week-17-python-knights-travail-long-practice/path_finder.py
@@ -0,0 +1,59 @@
+from tree import Node
+
+class KnightPathFinder:
+    def __init__(self, start_pos):
+        # Verify
+        try:
+            if type(start_pos) is not tuple:
+                raise Exception("Start coordinates must be a tuple")
+            elif not isinstance(start_pos[0], int) or not isinstance(start_pos[1], int):
+                raise Exception("Start coordinate values must be integers")
+            elif 0 < start_pos[0] >= 8 or 0 < start_pos[1] >= 8:
+                raise Exception("Coordinates must be between 0 and 7 for a standard 8x8 chess board")
+
+            # Set values if the start coordinates passed in pass checks for validity
+            self._root = Node(start_pos)
+            self._considered_positions = { start_pos }
+            # print(self._root, self._root.value)
+            # print(self._considered_positions)
+
+        except Exception as e:
+            print("Error:", e)
+
+    def get_valid_moves(self, pos):
+        x, y = pos
+        valid_moves = set()
+
+        # There are up to 8 possible valid moves for the knight depending upon it's position on the board
+        possible_moves = [
+            (1, 2),
+            (-1, 2),
+            (2, 1),
+            (2, -1),
+            (1, -2),
+            (-1, -2),
+            (-2, -1),
+            (-2, 1)
+        ]
+
+        for move in possible_moves:
+            new_x, new_y = x + move[0], y + move[1]
+
+            # Add to valid moves list only if the values are non-negative and less than board boundary (8)
+            if 0 <= new_x < 8 and 0 <= new_y < 8:
+                valid_moves.add((new_x, new_y))
+
+        return valid_moves
+
+
+    def new_move_positions(self, pos):
+        valid_moves = self.get_valid_moves(pos)
+
+        new_moves = valid_moves.difference(self._considered_positions)
+        self._considered_positions.update(new_moves)
+
+        return new_moves
+
+
+# finder = KnightPathFinder((0,0))
+# print(finder.new_move_positions((0,0)))
