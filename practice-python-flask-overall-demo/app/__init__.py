ae52f2f74c87e3285b78b592d477daeccdd80ff2
Ken Sully <ken.sully@gmail.com>
2024-11-13 13:09:37 -0700

    Completed python flask overall demo program - incorporates basic routing, POST handling, Jinja templates, form data with wtforms and flask-wtf, blueprints, and sessions

---
+++ practice-python-flask-overall-demo/app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from app.config import Config
+from app import routes
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.register_blueprint(routes.main.bp)
+app.register_blueprint(routes.inventory.bp)
