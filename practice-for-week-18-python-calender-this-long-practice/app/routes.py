740488934b778949b68c6e1da13076248e037a06
Ken Sully <ken.sully@gmail.com>
2024-11-14 11:05:07 -0700

    Completed python calendar-this long practice exercise main. May come back to complete stretch goals at a later time

--- practice-for-week-18-python-calender-this-long-practice/app/routes.py
+++ practice-for-week-18-python-calender-this-long-practice/app/routes.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
-from datetime import datetime
-from flask import Blueprint, render_template, redirect
+from datetime import datetime, timedelta
+from flask import Blueprint, render_template, redirect, url_for
 from app.forms.forms import AppointmentForm
 
 bp = Blueprint('main', __name__, '/')
@@ -16,6 +16,12 @@ CONNECTION_PARAMETERS = {
 
 @bp.route('/', methods=['GET', 'POST'])
 def main():
+    current_datetime = datetime.now()
+    daily_url = url_for('.daily', year=current_datetime.year, month=current_datetime.month, day=current_datetime.day)
+    return redirect(daily_url)
+
+@bp.route('/<int:year>/<int:month>/<int:day>', methods=['GET', 'POST'])
+def daily(year, month, day):
     form = AppointmentForm()
     if form.validate_on_submit():
         params = {
@@ -35,13 +41,22 @@ def main():
                             params) # use params for all of the parameterized values as it is already a dict
 
         return redirect('/')
+
+    today = datetime(year, month, day)
+    next_day = today + timedelta(days=1)
+    print(f'\n{today}\n{next_day}\n')
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
             curs.execute("""
                          SELECT id, name, start_datetime, end_datetime
                          FROM appointments
+                         WHERE start_datetime BETWEEN %(today)s AND %(next_day)s
                          ORDER BY start_datetime;
-                         """)
+                         """,
+                         {
+                             'today': today,
+                             'next_day': next_day
+                         })
             rows = curs.fetchall()  # this will be a list of tuples
 
     return render_template('main.html', rows=rows, form=form)
