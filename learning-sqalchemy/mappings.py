01e9cc881f54b9c0f4def50b8e60c250832034e6
Ken Sully <ken.sully@gmail.com>
2024-11-18 12:10:14 -0700

    WIP Completed python sqlalchemy create update and delete demo exercise

--- learning-sqalchemy/mappings.py
+++ learning-sqalchemy/mappings.py
@@ -1,9 +1,27 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.schema import Column, ForeignKey, Table
 from sqlalchemy.types import Integer, String
 
 Base = declarative_base()
+
+# CREATE TABLE pony_handlers (
+#   pony_id INTEGER NOT NULL,
+#   handler_id INTEGER NOT NULL,
+#   PRIMARY KEY (pony_id, handler_id),
+#   FOREIGN KEY (pony_id) REFERENCES ponies(id),
+#   FOREIGN KEY (handler_id) REFERENCES handlers(id)
+# );
+
+# Informs SQLAlchemy about the many-to-many relationship using the join table pony_handlers
+# No mapping required as the pony_handlers table exists only to establish the relationship
+pony_handlers = Table(
+    "pony_handlers",
+    Base.metadata,
+    Column("pony_id", ForeignKey("ponies.id"), primary_key=True),
+    Column("handler_id", ForeignKey("handlers.id"), primary_key=True))
+
+# Pony and Owner declarations below...
 
 # CREATE TABLE owners (
 #   id SERIAL PRIMARY KEY,
@@ -19,9 +37,9 @@ class Owner(Base):
     last_name = Column(String(255))
     email = Column(String(255))
 
-    ponies = relationship("Pony", back_populates="owner")
-
-
+    ponies = relationship("Pony",                       # Establishes the one to many relationship between owner and ponies
+                          back_populates="owner",
+                          cascade="all, delete-orphan")
 
 
 # CREATE TABLE ponies (
@@ -41,4 +59,38 @@ class Pony(Base):
     breed = Column(String(255))
     owner_id = Column(Integer, ForeignKey("owners.id"))
 
-    owner = relationship("Onwer", back_populates="ponies")
+    owner = relationship("Owner", back_populates="ponies")  # Establishes the one to many relationship ponies and owner
+    handlers = relationship("Handler",                      # Establishes the many-to-many relationship using the pony_handlers entity-less table mappings
+                            secondary=pony_handlers,
+                            back_populates="ponies")
+
+
+class Handler(Base):
+    __tablename__ = "handlers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    employee_id = Column(String(12))
+
+    ponies = relationship("Pony",
+                          secondary=pony_handlers,
+                          back_populates="handlers")
+
+# # Demonstration Code showing that back_populates will update the other attribute if you only update one
+# # Create an owner
+# owner = Owner(first_name="Lord",
+#               last_name="Dogknife",
+#               email="doggy@poodles.net")
+
+# # Create a pony
+# pony = Pony(name="Walking Walker",
+#             birth_year=3023,
+#             breed="Cyborg")
+
+# # Set the pony's owner
+# pony.owner = owner
+
+# # MAGIC: SQLAlchemy adds it to the ponies list!
+# print(owner.ponies)
+# # Prints [<Pony ...>]
--- learning-sqalchemy/pony_owners.py
+++ learning-sqalchemy/pony_owners.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from mappings import Owner, Pony
 
 db_url = "postgresql://sqlalchemy_test:password@localhost/sqlalchemy_test"
 engine = create_engine(db_url)
@@ -10,17 +11,65 @@ session = SessionFactory()
 
 # Do stuff with the session
 
-with engine.connect() as connection:
-    query = text("""
-            SELECT o.first_name, o.last_name, p.name
-            FROM owners o
-            JOIN ponies p ON (o.id = p.owner_id)
-            """)
+# # Create values to insert
+you = Owner(first_name="neu",
+            last_name="kenio",
+            email="neukenio@nope.com")
 
-    result = connection.execute(query)
+your_pony = Pony(name="Pon Pon",
+                 birth_year=2020,
+                 breed="Horsie",
+                 owner=you)
 
-    for row in result:
-        row_as_dict = row._mapping  # Need to add this as sqlalchemy will return a tuple
-        print(row_as_dict["first_name"], row_as_dict["last_name"], "owns", row_as_dict["name"])
+print(you.id)           # > None
+print(your_pony.id)     # > None
 
+# with engine.connect() as connection:
+# #     Select query
+#     query = text("""
+#             SELECT o.first_name, o.last_name, p.name
+#             FROM owners o
+#             JOIN ponies p ON (o.id = p.owner_id)
+#             """)
+
+#     result = connection.execute(query)
+
+#     for row in result:
+#         row_as_dict = row._mapping  # Need to add this as sqlalchemy will return a tuple
+#         print(row_as_dict["first_name"], row_as_dict["last_name"], "owns", row_as_dict["name"])
+
+# engine.dispose()
+
+# The Session object has already been created and bound to the engine.
+session.add(you)      # Connects you and your_pony objects
+session.commit()      # Saves data to the database
+
+print(you.id)         # > 4 (or whatever the new id is)
+print(your_pony.id)   # > 4 (or whatever the new id is)
+
+print(your_pony.birth_year)    # > 2020
+
+your_pony.birth_year = 2019
+print(your_pony.birth_year)    # > 2019
+
+session.commit()
+
+print(your_pony.birth_year)    # > 2019
+
+# NEW CODE BEGINS
+your_pony.name = "Mr. Fancy Pants"
+your_pony.birth_year = 1896
+print(your_pony.name)          # > Mr. Fancy Pants
+print(your_pony.birth_year)    # > 1896
+
+session.rollback()
+print(your_pony.name)          # > your pony's original name
+print(your_pony.birth_year)    # > 2019
+# NEW CODE ENDS
+
+# Continuing the code from above...
+session.delete(you)
+session.commit()
+
+session.close()
 engine.dispose()
