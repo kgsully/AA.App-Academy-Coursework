b661ce7d3442a76dce87fb54024bca0ae427c9f2
Ken Sully <ken.sully@gmail.com>
2024-11-19 13:04:04 -0700

    WIP Order Up long practice exercise: Added db models for menus, menu items, and menu item types. Seeded DB with temp data for these 3 tables (via database.py script)

--- practice-for-week-18-python-order-up-long-practice/app/models.py
+++ practice-for-week-18-python-order-up-long-practice/app/models.py
@@ -23,3 +23,41 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+# Menu has a one-to-many relationship with named items to the MenuItem objects
+# A menu can have many items, but each item is assigned to a particular menu
+class Menu(db.Model):
+    __tablename__ = "menus"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    items = db.relationship("MenuItem", back_populates="menu", cascade="all, delete-orphan")
+
+
+# MenuItem has a many-to-one relationship with Menu, and a many-to-one relationship with MenuItemType
+# Many menu items can be part of a particular menu
+# There are many menu items, but each item has one particular menu item type
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False)
+
+    menu = db.relationship("Menu", back_populates="items")
+    type = db.relationship("MenuItemType", back_populates="menu_item")
+
+
+# MenuItemType has a many-to-one relationship with MenuItem
+# Many menu items have a type (read: all menu items have a type), but each menu item can have only one type
+class MenuItemType(db.Model):
+    __tablename__ = "menu_item_types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    menu_item = db.relationship("MenuItem", back_populates="type", cascade="all, delete-orphan")
--- practice-for-week-18-python-order-up-long-practice/database.py
+++ practice-for-week-18-python-order-up-long-practice/database.py
@@ -4,13 +4,39 @@ load_dotenv()
 # Regardless of the lint error you receive, load_dotenv must run before running this
 # so that the environment variables are properly loaded.
 from app import app, db
-from app.models import Employee
+from app.models import Employee, Menu, MenuItem, MenuItemType
 
 
 with app.app_context():
+    # Drop and create all tables
     db.drop_all()
     db.create_all()
 
+    # Handle seed data for "employees" table
     employee = Employee(name="Margot", employee_number=1234, password="password")
+
+
+    # Handle seed data for "menus", "menu_items", and "menu_item_types" tables
+    beverages = MenuItemType(name="Beverages")
+    entrees = MenuItemType(name="Entrees")
+    sides = MenuItemType(name="Sides")
+
+    dinner = Menu(name="Dinner")
+
+    fries = MenuItem(name="French fries", price=3.50, type=sides, menu=dinner)
+    drp = MenuItem(name="Dr. Pepper", price=1.0, type=beverages, menu=dinner)
+    jambalaya = MenuItem(name="Jambalaya", price=21.98, type=entrees, menu=dinner)
+
+
+    # Add data to sessions before committing
     db.session.add(employee)
+    db.session.add(beverages)
+    db.session.add(entrees)
+    db.session.add(sides)
+    db.session.add(dinner)
+    db.session.add(fries)
+    db.session.add(drp)
+    db.session.add(jambalaya)
+
+    # Commit data from session
     db.session.commit()
