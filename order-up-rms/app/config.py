43f7609c16ccae1cdfc67f942e867c6f47e9a2d4
Ken Sully <ken.sully@gmail.com>
2024-11-19 11:06:40 -0700

    WIP Renamed folder for order-up long practice exercise

--- order-up-rms/app/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Configuration(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY")
---
+++ practice-for-week-18-python-order-up-long-practice/app/models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Employee(db.Model, UserMixin):
+    __tablename__ = "employees"
