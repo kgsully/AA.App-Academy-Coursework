f0783c049020b9efdd759eae7f40cde112279e71
Ken Sully <ken.sully@gmail.com>
2024-12-05 10:54:34 -0700

    compelted node flask microservices long practice exercise phase 2

--- practice-for-week-19-node-flask-microservices/flask_app/book_ratings.py
+++ practice-for-week-19-node-flask-microservices/flask_app/book_ratings.py
@@ -1,11 +1,7 @@
-from app import app
-
 from flask.cli import FlaskGroup
+from app import app
 
 cli = FlaskGroup(app)
 
 if __name__ == "__main__":
     cli()
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
